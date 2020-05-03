916c8c8d8c2c1a5b6191ad80181fcc2b6cb9d05d
alaguselvan <alaguselvan97@gmail.com>
2020-05-03 22:08:30 +0530

    microservice setup over

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -20,13 +20,14 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending',
     }
   })
   commentsByPostId[req.params.id] = comments;
@@ -34,8 +35,35 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log(`Event Received:`, req.body.type)
+
+  const { type, data } = req.body;
+
+  if(type === 'CommentModerated') {
+    const { postId, id, status, content } = data
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => {
+      return comment.id === id
+    });
+    comment.status = status;
+try {
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentUpdated',
+    data: {
+      id,
+      status,
+      postId,
+      content
+    },
+  });
+} catch (e) {
+  console.log(e.message)
+  console.log('Comment Service Error')
+}
+
+  }
   res.send({});
 })
 
