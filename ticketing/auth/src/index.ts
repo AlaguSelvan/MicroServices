7275c2c1cb01b5a26e4544e74503282e6477962e
alaguselvan <alaguselvan97@gmail.com>
2020-05-07 00:38:15 +0530

    added auth setup and added mongo k8s setup

---
+++ ticketing/auth/src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error'
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+
+app.all('*', async (req: Request, res: Response) => {
+  throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log('Auth Listening on 3000!')
+})
---
+++ ticketing/auth/src/routes/current-user.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response} from 'express';
+
+const router = express.Router()
+
+router.get(
+  '/api/users/currentuser',
+  async(req: Request, res: Response) => {
+    res.send('Hi There!');
+  }
+);
+
+export { router as currentUserRouter };
