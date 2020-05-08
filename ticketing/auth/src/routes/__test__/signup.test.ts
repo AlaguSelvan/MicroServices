a074e9fbbfb4c1fdb9cc734475d828cf3806ae05
alaguselvan <alaguselvan97@gmail.com>
2020-05-08 20:49:46 +0530

    added nextjs frontend for ticketing, added unit tests for auth service

--- ticketing/auth/src/routes/__test__/signup.test.ts
+++ ticketing/auth/src/routes/__test__/signup.test.ts
@@ -2,7 +2,6 @@ import request from 'supertest'
 import { app } from '../../app'
 
 it('returns a 201 on successful signup', async() => {
-  console.log(process.env.JWT_KEY!)
   return request(app)
     .post('/api/users/signup')
     .send({
@@ -11,3 +10,67 @@ it('returns a 201 on successful signup', async() => {
     })
     .expect(201)
 })
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test1gmail.com',
+      password: 'password',
+    })
+    .expect(400);
+})
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@gmail.com',
+      password: '1',
+    })
+    .expect(400);
+})
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+    })
+    .expect(400);
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'abc12345',
+    })
+    .expect(400);
+})
+
+
+it('disallows duplicate emails', async() => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    }).expect(201);
+  
+  await request(app)
+  .post('/api/users/signup')
+  .send({
+    email: 'test@test.com',
+    password: 'password'
+  }).expect(400);
+})
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+  
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,17 +1,10 @@
-import express, { Request, Response} from 'express';
-import jwt from 'jsonwebtoken'
+import express from 'express';
 import { currentUser } from '../middlewares/current-user';
-import { requireAuth } from '../middlewares/require-auth';
 
-const router = express.Router()
+const router = express.Router();
 
-router.get(
-  '/api/users/currentuser',
-  currentUser,
-  requireAuth,
-  async(req: Request, res: Response) => {
-    res.send({ currentUser: req.currentUser || null });
-  }
-);
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+});
 
 export { router as currentUserRouter };
