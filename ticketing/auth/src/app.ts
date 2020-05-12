a86c6c2dd4d65eeddd06461efb2b85b8f34f86f8
alaguselvan <alaguselvan97@gmail.com>
2020-05-12 18:07:00 +0530

    nats setup

--- ticketing/auth/src/app.ts
+++ ticketing/auth/src/app.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response } from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
-
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@sftickets/common';
-import cookieSession from 'cookie-session'
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,20 +15,19 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: process.env.NODE_ENV !== 'test',
   })
 );
 
-
 app.use(currentUserRouter);
-app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
+app.use(signupRouter);
 
-app.all('*', async (req: Request, res: Response) => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
-})
+});
 
 app.use(errorHandler);
 
-export { app }
+export { app };
